d685705805b8c4d7d234848188e0ca490a29833e
Hajar Elamiry <elamiryhajar801@gmail.com>
2026-03-31 12:50:00 +0100

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Providers from './providers'
 
 export const metadata: Metadata = {
     icons: { icon: "/favicon.svg" },
+    viewport: 'width=device-width, initial-scale=1',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
